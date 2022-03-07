b88eb1bcdf0cff8c9c82b94524e0a7c75a7c6935
FlavienBuron <flavien.buron@gmail.com>
2022-03-07 10:21:50 +0100

    completed benchmark

--- install_extra.sh
+++ install_extra.sh
@@ -1,8 +1,15 @@
 pip2 install numpy scipy pandas sklearn
-pip2 install tensorflow
+pip2 install tensorflow==1.14.0
 pip2 install torch torchvision
 pip2 install pandas
 pip2 install ujson
 pip2 install ipdb
 pip2 install scikit-learn
 pip2 install future
+
+# python3 dependencies (for DeepMVI)
+sudo apt-get update
+sudo apt-get install python3.6
+sudo apt install python3-venv python3-pip
+python3.6 -m pip install torch
+python3.6 -m pip install numpy
