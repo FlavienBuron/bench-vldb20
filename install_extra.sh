8645b766fc80e0286d4711f695b7483a832dc3dc
FlavienBuron <flavien.buron@gmail.com>
2022-05-09 12:05:45 +0200

    Added installation of python3.8 for PartialGan implementation (compatibilitie issues)

--- install_extra.sh
+++ install_extra.sh
@@ -7,11 +7,18 @@ pip2 install ipdb
 pip2 install scikit-learn
 pip2 install future
 
-# python3 dependencies (for DeepMVI)
+# python3 dependencies. 2 different versions of python 3 are used to fight compatibilities issues between the different
+# Tensorflow/Keras version needed by the different models
 sudo apt-get update
+sudo apt install software-properties-common
+sudo add-apt-repository ppa:deadsnakes/ppa -y
 sudo apt-get install python3.6
-sudo apt-get install python3.6-distutils
+sudo apt install python3.8
 sudo apt install python3-venv python3-pip
+# python 3.6 packages
 python3.6 -m pip install torch
 python3.6 -m pip install numpy
 python3.6 -m pip install tensorflow==1.7.0
+# python3.8 packages
+python3.8 -m pip install tensorflow
+python3.8 -m pip install numpy
