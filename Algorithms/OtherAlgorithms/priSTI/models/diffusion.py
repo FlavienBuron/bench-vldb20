04bde5c39a7296089fae25426812e7e091f502ef
FlavienBuron <flavien.buron@gmail.com>
2024-09-10 12:25:07 +0200

    Changed fake adjacency matrix to ones instead of zeros

--- Algorithms/OtherAlgorithms/priSTI/models/diffusion.py
+++ Algorithms/OtherAlgorithms/priSTI/models/diffusion.py
@@ -9,12 +9,22 @@ class Guide_diff(nn.Module):
         self.itp_channels = None
         if self.is_itp:
             self.itp_channels = config["channels"]
-            self.itp_projection = Conv1d_with_init(inputdim-1, self.itp_channels, 1)
+            self.itp_projection = Conv1d_with_init(inputdim - 1, self.itp_channels, 1)
 
-            self.itp_modeling = GuidanceConstruct(channels=self.itp_channels, nheads=config["nheads"], target_dim=target_dim,
-                                            order=2, include_self=True, device=config["device"], is_adp=config["is_adp"],
-                                            adj_file=config["adj_file"], proj_t=config["proj_t"])
-            self.cond_projection = Conv1d_with_init(config["side_dim"], self.itp_channels, 1)
+            self.itp_modeling = GuidanceConstruct(
+                channels=self.itp_channels,
+                nheads=config["nheads"],
+                target_dim=target_dim,
+                order=2,
+                include_self=True,
+                device=config["device"],
+                is_adp=config["is_adp"],
+                adj_file=config["adj_file"],
+                proj_t=config["proj_t"],
+            )
+            self.cond_projection = Conv1d_with_init(
+                config["side_dim"], self.itp_channels, 1
+            )
             self.itp_projection2 = Conv1d_with_init(self.itp_channels, 1, 1)
 
         self.diffusion_embedding = DiffusionEmbedding(
@@ -22,14 +32,18 @@ class Guide_diff(nn.Module):
             embedding_dim=config["diffusion_embedding_dim"],
         )
 
-        self.adj = np.zeros((target_dim, target_dim))
+        self.adj = np.ones((target_dim, target_dim))
         self.device = config["device"]
         self.support = compute_support_gwn(self.adj, device=config["device"])
         self.is_adp = config["is_adp"]
         if self.is_adp:
             node_num = self.adj.shape[0]
-            self.nodevec1 = nn.Parameter(torch.randn(node_num, 10).to(self.device), requires_grad=True).to(self.device)
-            self.nodevec2 = nn.Parameter(torch.randn(10, node_num).to(self.device), requires_grad=True).to(self.device)
+            self.nodevec1 = nn.Parameter(
+                torch.randn(node_num, 10).to(self.device), requires_grad=True
+            ).to(self.device)
+            self.nodevec2 = nn.Parameter(
+                torch.randn(10, node_num).to(self.device), requires_grad=True
+            ).to(self.device)
             self.support.append([self.nodevec1, self.nodevec2])
 
         self.input_projection = Conv1d_with_init(inputdim, self.channels, 1)
@@ -56,7 +70,6 @@ class Guide_diff(nn.Module):
             ]
         )
 
-
     def forward(self, x, side_info, diffusion_step, itp_x, cond_mask):
         if self.is_itp:
             x = torch.cat([x, itp_x], dim=1)
@@ -68,7 +81,7 @@ class Guide_diff(nn.Module):
         x = x.reshape(B, self.channels, K, L)
 
         if self.is_itp:
-            itp_x = itp_x.reshape(B, inputdim-1, K * L)
+            itp_x = itp_x.reshape(B, inputdim - 1, K * L)
             itp_x = self.itp_projection(itp_x)
             itp_cond_info = side_info.reshape(B, -1, K * L)
             itp_cond_info = self.cond_projection(itp_cond_info)
@@ -81,7 +94,9 @@ class Guide_diff(nn.Module):
 
         skip = []
         for i in range(len(self.residual_layers)):
-            x, skip_connection = self.residual_layers[i](x, side_info, diffusion_emb, itp_x, self.support)
+            x, skip_connection = self.residual_layers[i](
+                x, side_info, diffusion_emb, itp_x, self.support
+            )
             skip.append(skip_connection)
 
         x = torch.sum(torch.stack(skip), dim=0) / math.sqrt(len(self.residual_layers))
@@ -94,24 +109,51 @@ class Guide_diff(nn.Module):
 
 
 class NoiseProject(nn.Module):
-    def __init__(self, side_dim, channels, diffusion_embedding_dim, nheads, target_dim, proj_t, order=2, include_self=True,
-                 device=None, is_adp=False, adj_file=None, is_cross_t=False, is_cross_s=True):
+    def __init__(
+        self,
+        side_dim,
+        channels,
+        diffusion_embedding_dim,
+        nheads,
+        target_dim,
+        proj_t,
+        order=2,
+        include_self=True,
+        device=None,
+        is_adp=False,
+        adj_file=None,
+        is_cross_t=False,
+        is_cross_s=True,
+    ):
         super().__init__()
         self.diffusion_projection = nn.Linear(diffusion_embedding_dim, channels)
         self.cond_projection = Conv1d_with_init(side_dim, 2 * channels, 1)
         self.mid_projection = Conv1d_with_init(channels, 2 * channels, 1)
         self.output_projection = Conv1d_with_init(channels, 2 * channels, 1)
 
-        self.forward_time = TemporalLearning(channels=channels, nheads=nheads, is_cross=is_cross_t)
-        self.forward_feature = SpatialLearning(channels=channels, nheads=nheads, target_dim=target_dim,
-                                               order=order, include_self=include_self, device=device, is_adp=is_adp,
-                                               adj_file=adj_file, proj_t=proj_t, is_cross=is_cross_s)
+        self.forward_time = TemporalLearning(
+            channels=channels, nheads=nheads, is_cross=is_cross_t
+        )
+        self.forward_feature = SpatialLearning(
+            channels=channels,
+            nheads=nheads,
+            target_dim=target_dim,
+            order=order,
+            include_self=include_self,
+            device=device,
+            is_adp=is_adp,
+            adj_file=adj_file,
+            proj_t=proj_t,
+            is_cross=is_cross_s,
+        )
 
     def forward(self, x, side_info, diffusion_emb, itp_info, support):
         B, channel, K, L = x.shape
         base_shape = x.shape
         x = x.reshape(B, channel, K * L)
-        diffusion_emb = self.diffusion_projection(diffusion_emb).unsqueeze(-1)  # (B,channel,1)
+        diffusion_emb = self.diffusion_projection(diffusion_emb).unsqueeze(
+            -1
+        )  # (B,channel,1)
         y = x + diffusion_emb
 
         y = self.forward_time(y, base_shape, itp_info)
@@ -133,4 +175,3 @@ class NoiseProject(nn.Module):
         skip = skip.reshape(base_shape)
 
         return (x + residual) / math.sqrt(2.0), skip
-
